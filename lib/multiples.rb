a8d1252c155353036a15e431d0efd356a9796cda
castagnaTM <castagna.tm@gmail.com>
2019-09-13 09:51:58 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+    arr = []
+    i = 1
+    while i < limit do 
+        arr << i if i%3 == 0 || i%5 == 0
+        i += 1
+    end
+    arr
+end
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end
