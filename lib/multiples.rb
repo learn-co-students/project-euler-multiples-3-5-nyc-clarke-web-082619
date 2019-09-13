ae9202416a300991670180dc239a278a7405115b
github username <github email address>
2019-09-13 09:50:24 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,13 @@
-# Enter your procedural solution here!
+def collect_multiples(limit)
+    a = []
+    (1...limit).each do |num|
+        if num % 3 == 0 || num % 5 == 0
+            a << num
+        end
+    end 
+    a
+end 
+
+def sum_multiples(limit)
+    collect_multiples(limit).sum
+end 
