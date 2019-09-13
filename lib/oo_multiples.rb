d7081e1779715a957008e00a9407a74295f1c8af
github username <github email address>
2019-09-13 09:21:53 -0400

    Done.

--- lib/oo_multiples.rb
+++ lib/oo_multiples.rb
@@ -1,1 +1,21 @@
 # Enter your object-oriented solution here!
+class Multiples
+    attr_accessor :n
+    def initialize(n)
+        @n = n
+    end
+
+    def collect_multiples
+        arr = []
+        for i in 1..(@n-1)
+            if i%3 == 0 || i%5 ==0
+                arr << i
+            end
+        end
+        arr
+    end
+
+    def sum_multiples
+        collect_multiples.sum
+    end
+end
